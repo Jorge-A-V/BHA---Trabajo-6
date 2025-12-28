25564b09c2ed2d9d48ccaa091d336fa86e9dca98
Jorge-A-V <131684563+Jorge-A-V@users.noreply.github.com>
2025-12-28 12:22:39 +0100

    creo que rta?

---
+++ algoritmos/rta.py
@@ -0,0 +1,47 @@
+from helpers.grid import grid
+import numpy as np
+
+class RTA:
+
+    def __init__(self, grid: grid):
+        self.grid = grid
+        self.gn = 0
+        self.traza = []
+        self.nodo_final = None
+
+    def run(self):
+        nodo = self.grid.inicio
+
+        while nodo:
+            nodo = self.step(nodo)
+            if self.grid.get.es_solucion(nodo):
+                break
+        
+        self.nodo_final = nodo
+
+    def step(self, id_nodo_actual):
+        nodo_actual = self.grid.get(id_nodo_actual)
+        vecinos = self.grid.vecinos(id_nodo_actual)
+
+        print(vecinos)
+        fns = [
+            self.fn(self.grid.h(vecino_id)) for vecino_id in vecinos
+        ]
+
+        pos_vecino_min = np.argmin(fns)
+
+        fn_min = fns[pos_vecino_min]
+        vecino_id_min = vecinos[pos_vecino_min]
+
+        self.grid.actualizar(id_nodo_actual, fn_min)
+        self.gn += 1
+        self.traza.append(id_nodo_actual)
+
+        siguiente_nodo = vecino_id_min
+
+        return siguiente_nodo
+
+    def fn(self, vecino_h):
+        return self.gn + 1 + vecino_h
+
+
