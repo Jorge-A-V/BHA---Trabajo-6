5f20d1676c7073a2bcaf340cba68b603007e4846
Jorge-A-V <131684563+Jorge-A-V@users.noreply.github.com>
2026-03-31 20:21:02 +0200

    run encadenados en el main

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 
-from helpers.grid import grid
+from helpers.grid import grid, posiciones_iniciales_extra
 from algoritmos.rta import RTA
+from algoritmos.lrta import LRTA
 
 if __name__ == "__main__":
-    grid_inicial = grid()
+    grid_rta = grid()
+    
+    print(grid_rta.grid)
 
-    print(grid_inicial.grid)
+    rta = RTA(grid_rta)
+    rta.run()
 
-    algo = RTA(grid_inicial)
+    for idx, posicion_inicial_extra in enumerate(posiciones_iniciales_extra):
+        grid_rta.cambiar_inicio_sin_cargar_dist(idx)
+        rta = RTA(grid_rta)
+        rta.run()
 
-    algo.run()
+    grid_lrta = grid()
+    lrta = LRTA(grid_lrta)
+    lrta.run()
 
-    print(f"traza: {algo.traza}")
+    for idx, posicion_inicial_extra in enumerate(posiciones_iniciales_extra):
+        grid_lrta.cambiar_inicio_sin_cargar_dist(idx)
+        lrta = LRTA(grid_lrta)
+        lrta.run()
+
